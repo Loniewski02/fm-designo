1bd51155dfb7ec7425a5672f58d92d3900736e80
Milosz <miloszloniewski02@gmail.com>
2024-06-03 14:37:13 +0200

    vercel deployment fixed

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+import About from "./components/About";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 
@@ -6,6 +7,7 @@ const page = () => {
     <>
       <Header />
       <Projects />
+      <About />
     </>
   );
 };
